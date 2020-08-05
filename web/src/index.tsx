389c6b4ba2222c7c94bcc2f9f671f7ce6b513238
ederdaniel22 <60990935+ederdaniel22@users.noreply.github.com>
2020-08-04 22:42:12 -0400

    Primeiro comit
    Criação da Landing page e estlização

---
+++ web/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/*Aqui podemos tirar o <App /> e colocar qualquer item html paara aparecer na tela */}
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
